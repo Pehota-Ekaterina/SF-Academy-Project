a60c79d5997c7dff2e2c4eaa473bbc5e7c0254a8
Pekhota_Ekaterina <73845600+Pehota-Ekaterina@users.noreply.github.com>
2025-11-28 17:52:14 +0300

    so so header

---
+++ force-app/main/default/lwc/header/header.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import aliexpressLogo from '@salesforce/resourceUrl/aliexpress';
+
+export default class Header extends LightningElement {
+    aliexpressLogo = aliexpressLogo;
+}
